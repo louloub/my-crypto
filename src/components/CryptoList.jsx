af792c8420fc7f298710836c656a1d7761ce8d05
loulou <yourdj.france@gmail.com>
2021-02-03 22:32:24 +0100

    working on actualise price of coin on database with online price

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,30 +1,13 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {CryptoContext} from "../context/CryptoContext";
 import CryptoTable from "./CryptoTable";
 
 const instance = axios.create({ baseURL: "http://localhost:5000/" });
 
 const CryptoList = props => {
-  const crypto = useContext(CryptoContext);
-
-  // Retrive crypto list on database and push it on context
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const cryptoList = await instance.get(`/cryptolist`);
-        console.log(cryptoList.data)
-        for (let i = 0; i < cryptoList.data.length; i++ ){
-            crypto.setList(crypto.list.push(cryptoList.data[i]))
-            // console.log(crypto.list)
-        } 
-      } catch (err) {}
-    }
-    fetchData();
-  }, []);
-
+    
   return (
-    <CryptoTable />
+    <CryptoTable/>
   );
 };
 
--- src/components/CryptoTable.jsx
+++ src/components/CryptoTable.jsx
@@ -1,97 +1,119 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useState, Button } from "react";
 import { Table } from "reactstrap";
 import axios from "axios";
 
-import CryptoContext from "../context/CryptoContext";
 let coinCeckoBaseUrl = "https://api.coingecko.com/api/v3/simple/price?ids=";
+const instance = axios.create({ baseURL: "http://localhost:5000/" });
 
 const CryptoTable = props => {
-  const crypto = useContext(CryptoContext);
-  console.log(crypto);
-  console.log(crypto.list)
+  const [cryptos, setCryptos] = useState([]);
 
-//   for (let i = 0; i < crypto.list.length; i++) {
-//     // crypto.setList(crypto.list.push(cryptoList.data[i]));
-//     console.log(crypto.list[i]);
-//   }
-  return "rien";
-  //   const list = useContext(CryptoContext);
-  //   console.log(list)
-  //   const setCrypto = useContext(CryptoContext);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const cryptoList = await instance.get(`/cryptolist`);
+        let newList = [];
+        for (let i = 0; i < cryptoList.data.length; i++) {
+          newList.push(cryptoList.data[i]);
+        }
+        setCryptos(newList);
+        retrieveCoinPrice();
+      } catch (err) {}
+    }
+    fetchData();
+  }, []);
 
-  //   async function retrieveCoinPrice() {
-  //     list.map(async crypto => {
-  //       let coinPrice = "";
-  //       const coinName = crypto.name.toLowerCase();
-  //       const coinCeckoFinalUrl =
-  //         coinCeckoBaseUrl + coinName + "&vs_currencies=USD";
-  //       try {
-  //         console.log(coinCeckoFinalUrl);
-  //         const coinInformation = await axios
-  //           .create({ baseURL: coinCeckoFinalUrl })
-  //           .get();
+  // Actualy just update BTC actual price with actual coingeck price
+  // on click button
+  async function updateAllAcutalPriceInDatabase(coinPrice, cryptoId) {
+    try {
+      const cryptoList = await instance.post(`/cryptolist`, {
+        id: cryptoId,
+        actualPrice: coinPrice
+      });
+    } catch (err) {}
+  }
 
-  //         switch (coinName) {
-  //           case "bitcoin":
-  //             coinPrice = coinInformation.data.bitcoin.usd;
-  //             setCrypto(crypto.actualPrice = coinPrice)
-  //             console.log(coinPrice);
-  //             break;
-  //           case "ethereum":
-  //             coinPrice = coinInformation.data.ethereum.usd;
-  //             console.log(coinPrice);
-  //             break;
-  //           case "litecoin":
-  //             coinPrice = coinInformation.data.litecoin.usd;
-  //             console.log(coinPrice);
-  //             break;
-  //         }
-  //         for (const property in crypto){
-  //             console.log(`${property}`)
-  //         }
-  //         // setCrypto()
-  //       } catch (err) {
-  //         console.log(err);
-  //       }
-  //     });
-  //   }
+  async function retrieveCoinPrice() {
+    if (cryptos.length > 0) {
+      cryptos.map(async (crypto, index) => {
+        try {
+          let coinPrice = "";
+          const coinName = crypto.name.toLowerCase();
+          const coinCeckoFinalUrl =
+            coinCeckoBaseUrl + coinName + "&vs_currencies=USD";
+          const coinInformation = await axios
+            .create({ baseURL: coinCeckoFinalUrl })
+            .get();
 
-  //   useEffect(async () => {
-  //     try {
-  //       retrieveCoinPrice();
-  //     } catch (err) {}
-  //   }, []);
+          switch (coinName) {
+            case "bitcoin":
+              coinPrice = coinInformation.data.bitcoin.usd;
+              await setCryptos([
+                ...cryptos,
+                (cryptos[index].actualPrice = coinPrice)
+              ]);
+              updateAllAcutalPriceInDatabase(coinPrice, cryptos[index].id);
+              break;
+            case "ethereum":
+              coinPrice = coinInformation.data.ethereum.usd;
+              await setCryptos([
+                ...cryptos,
+                (cryptos[index].actualPrice = coinPrice)
+              ]);
+              updateAllAcutalPriceInDatabase(coinPrice, cryptos[index].id);
+              break;
+            case "litecoin":
+              coinPrice = coinInformation.data.litecoin.usd;
+              await setCryptos([
+                ...cryptos,
+                (cryptos[index].actualPrice = coinPrice)
+              ]);
+              updateAllAcutalPriceInDatabase(coinPrice, cryptos[index].id);
+              break;
+          }
+          for (const property in crypto) {
+            // console.log(`${property}: ${crypto[property]}`);
+            //  console.log(`${property}: ${object[property]}`);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      });
+    }
+  }
 
-  //   if (list != undefined) {
-  //     return (
-  //       <Table responsive>
-  //         <thead>
-  //           <tr>
-  //             <th>Name</th>
-  //             <th>Coin</th>
-  //             <th>Actual Price</th>
-  //             <th>Type</th>
-  //             <th>Market cap</th>
-  //           </tr>
-  //         </thead>
-  //         <tbody>
-  //           {list.map(crypto => {
-  //             return (
-  //               <tr>
-  //                 <td>{crypto.name}</td>
-  //                 <td>{crypto.coin}</td>
-  //                 <td>Actual price</td>
-  //                 <td>{crypto.type}</td>
-  //                 <td>Market cap</td>
-  //               </tr>
-  //             );
-  //           })}
-  //         </tbody>
-  //       </Table>
-  //     );
-  //   } else {
-  //     return "no coin actually";
-  //   }
+  if (cryptos != undefined) {
+    return (
+      <Table responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Coin</th>
+            <th>Actual Price</th>
+            <th>Type</th>
+            <th>Market cap</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cryptos.map((crypto, index) => {
+            return (
+              <tr>
+                <td>{crypto.name}</td>
+                <td>{crypto.coin}</td>
+                <td>{crypto.actualPrice}</td>
+                <td>{crypto.type}</td>
+                <td>Market cap</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <button onClick={retrieveCoinPrice}> update price</button>
+      </Table>
+    );
+  } else {
+    return "no coin actually";
+  }
 };
 
 export default CryptoTable;
